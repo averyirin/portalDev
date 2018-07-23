7aab041da4f54f3c4a5526e0f72ddcb4306ed92d
Irin <mpg.students+borden@gmail.com>
2018-07-23 10:38:44 -0400

    landing page ui

--- project_registry/js/views/request_support.php
+++ project_registry/js/views/request_support.php
@@ -1,20 +1,20 @@
 <?php ?>
 <div class='template-header col-lg-12'>
-    <h2><?php echo elgg_echo('projects:all'); ?></h2>
+    <h2><?php echo elgg_echo('projects:requestSupport'); ?></h2>
 
 </div>
 
-<section class='col-md-2'></section>
-<section class='col-md-4'>
+<section class='col-md-1'></section>
+<section class='col-md-5'>
 
       <h2><?php echo elgg_echo('Create Task'); ?></h2>
 
     <div>Tasks are anything that is less than 80 hrs of work</div>
     <a href='#/projects/create_task' class='elgg-button elgg-button-action float-left'><?php echo elgg_echo('tasks:create:task'); ?></a>
 </section>
-<section class='col-md-4 projects'>
+<section class='col-md-5 projects'>
 
-      <h2><?php echo elgg_echo('Create Project'); ?></h2>
+      <h1><?php echo elgg_echo('Create Project'); ?></h2>
       <div>Projects are more than 80 hrs of work. Require scope, cost, time, and risk estimates</div>
       <a href='#/projects/create' class='elgg-button elgg-button-action float-left'><?php echo elgg_echo('projects:create:project'); ?></a>
 
