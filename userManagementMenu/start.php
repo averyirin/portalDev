8e24f6356d669e2de9b4820f803577983245c5a8
Irin <mpg.students+borden@gmail.com>
2018-07-24 10:39:10 -0400

    user management menu

---
+++ userManagementMenu/start.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Elgg Webservice plugin
+ *
+ * @package Webservice
+ * @author Mark Harding (based on work started by Saket Saurabh)
+ *
+ */
+function userManagementMenuInit() {
+  if (elgg_is_admin_logged_in() && ((elgg_get_context()=='admin')) {
+$item = new ElggMenuItem('test', "Test Link", elgg_get_site_url() . 'usermgmt/all');
+elgg_register_menu_item('page',$item);
+
+}
+}
+
+elgg_register_event_handler('init', 'system', 'userManagementMenuInit');
