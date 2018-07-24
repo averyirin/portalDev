7d7cc4d16afeb2c31634c318c4e90dfd548238c2
Irin <mpg.students+borden@gmail.com>
2018-07-24 11:51:08 -0400

    elgg menu

--- userManagementMenu/start.php
+++ userManagementMenu/start.php
@@ -10,8 +10,8 @@ function userManagementMenuInit() {
 
   if (elgg_is_admin_logged_in() && (elgg_get_context()=='admin')) {
 
-      $item = new ElggMenuItem('administer', "Test Link", elgg_get_site_url().'usermgmt/all');
-      elgg_register_menu_item('page',$item);
+      $item = new ElggMenuItem('test', "Test Link", elgg_get_site_url().'usermgmt/all');
+      elgg_register_menu_item('page-administer',$item);
   }
   /*
     $item = new ElggMenuItem('test', "Test Link", elgg_get_site_url().'usermgmt/all');
