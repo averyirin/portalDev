fe8431bb25e65a48504d78fdd9d9d77c4dd402a5
Irin <mpg.students+borden@gmail.com>
2018-07-25 15:25:32 -0400

    task view

--- project_registry/js/views/list.php
+++ project_registry/js/views/list.php
@@ -52,10 +52,13 @@
                              </thead>
                              <tbody>
                                  <tr ng-repeat='(key,project) in vm.projects'>
+                                      <td>
+                                        {{project.classification}}
+                                      </td>
 
                                      <td>
                                        <a ng-if="{{project.classification =='Task'}}"  href='#/projects/view_task/{{project.id}}'>{{project.title}}</a>
-                                       <a ng-if="{{project.classification !='Task'}}"  href='#/projects/view/{{project.id}}'>{{project.title}}</a>
+                                       <a   href='#/projects/view/{{project.id}}'>{{project.title}}</a>
                                      </td>
 
 
