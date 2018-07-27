1ab8c8a4a8fb7526f13b7da4a59e71ba42f2e4b1
Irin <mpg.students+borden@gmail.com>
2018-07-27 11:04:34 -0400

    task creation required fields

--- wettoolkit/js/controllers/ProjectController.js
+++ wettoolkit/js/controllers/ProjectController.js
@@ -117,7 +117,7 @@
             angular.forEach(vm.project, function (value, key) {
                 vm[key] = value;
             });
-            vm.project.timeline = new Date(vm.project.timeline);
+            vm.project.timeline = new Date(null);
             vm.project.description = $sce.trustAsHtml(vm.project.description);
 
             //create slider for percentage complete
